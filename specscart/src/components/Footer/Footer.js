835d066dfe2acb202b977814f862af17520900e3
Amrut Sagar <amrutsagar06@gmail.com>
2022-09-29 20:15:19 +0530

    home page

--- specscart/src/components/Footer/Footer.js
+++ specscart/src/components/Footer/Footer.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="skills">Footer 1</div>
+      <div className="experience">
+        Footer 2
+        <div>
+          <div className="left-container">
+            <div className="point1">one</div>
+            <div className="point2">two</div>
+            <div className="point3">thhre</div>
+            <div className="point4">foour</div>
+          </div>
+
+          <div className="right-container">
+            <div className="point5">fivve</div>
+            <div className="point6">six</div>
+            <div className="point7">seven</div>
+            <div className="point8">eight</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- specscart/src/components/Home/Home.js
+++ specscart/src/components/Home/Home.js
@@ -1,35 +1,43 @@
 import React from "react";
 import "./Home.css";
+import lens from "../../images/Group6248.png";
+import pen from "../../images/Image35.png";
+import specs from "../../images/Image39.png";
 
 const Home = () => {
   return (
     <div>
-      <div class="bimg">
-        <div>
-          <div class="home_trial">Home Trial Cart 2.0</div>
-          <div class="its_an">It's An Experience!</div>
-          <div class="its_an">Four Frames. Three Lenses. 1Laser</div>
-          <div class="its_an">
-            Try Now <span>Watch Whats New!</span>
+      <div className="bimg">
+        <div className="homi">
+          <div className="home1">
+            <div className="home_trial">Home Trial Cart 2.0</div>
+            <div className="its_an">It's An Experience!</div>
+            <div className="its_an">Four Frames. Three Lenses. 1Laser</div>
+            <div className="its_an">
+              <a href="#" className="ctn">
+                Try Now
+              </a>
+            </div>
+          </div>
+          <div className="home1">
+            {/* <img src={lens} alt="i1" /> */}
+            {/* <img src={pen} alt="i2" /> */}
+            <img className="specs" src={specs} alt="i3" />
           </div>
         </div>
       </div>
-      <div class="aboutme">About Me</div>
-      <div class="mydetails">
-        I am an experienced content writer and digital marketing expert, with a
-        proven track record of writing engaging, informative content.
-      </div>
-      <div class="icons">
-        <a href="#" class="fa fa-twitter"></a>
 
-        <a href="#" class="fa fa-linkedin"></a>
-        <a href="#" class="fa fa-youtube"></a>
+      <div className="aboutme">Page One</div>
+      <div className="mydetails">Texts for page One</div>
+      <div className="icons">
+        <a href="#" className="fa fa-twitter"></a>
+
+        <a href="#" className="fa fa-linkedin"></a>
+        <a href="#" className="fa fa-youtube"></a>
       </div>
-      <div class="myprojects">My Projects</div>
-      <div class="pdetails">
-        I have build a Teacher Reviewing App ,Insight Guru using React Js.
-      </div>
-      <div class="also">
+      <div className="myprojects">Page 2 </div>
+      <div className="pdetails">Texts for Page 2</div>
+      <div className="also">
         Also have build front end of an Ecommerce App, Imdb App
       </div>
     </div>
