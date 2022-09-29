3b80711d1ba55bfc12a9ae5fa110daca831db9ac
Amrut Sagar <amrutsagar06@gmail.com>
2022-09-29 16:49:53 +0530

    header and home layout

---
+++ specscart/src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav class="navbar">
+      <h1 class="logo">Specscart</h1>
+      <ul class="nav-links">
+        <li>
+          <a href="#"></a>Glasses
+        </li>
+        <li>
+          <a href="#"></a>Sunglasses
+        </li>
+
+        <li>
+          <a href="#"></a>Eye-Test
+        </li>
+        <li>
+          <a href="#"></a>Blogs
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ specscart/src/components/Home/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div class="bimg">
+        <div>
+          <div class="home_trial">Home Trial Cart 2.0</div>
+          <div class="its_an">It's An Experience!</div>
+          <div class="its_an">Four Frames. Three Lenses. 1Laser</div>
+          <div class="its_an">
+            Try Now <span>Watch Whats New!</span>
+          </div>
+        </div>
+      </div>
+      <div class="aboutme">About Me</div>
+      <div class="mydetails">
+        I am an experienced content writer and digital marketing expert, with a
+        proven track record of writing engaging, informative content.
+      </div>
+      <div class="icons">
+        <a href="#" class="fa fa-twitter"></a>
+
+        <a href="#" class="fa fa-linkedin"></a>
+        <a href="#" class="fa fa-youtube"></a>
+      </div>
+      <div class="myprojects">My Projects</div>
+      <div class="pdetails">
+        I have build a Teacher Reviewing App ,Insight Guru using React Js.
+      </div>
+      <div class="also">
+        Also have build front end of an Ecommerce App, Imdb App
+      </div>
+    </div>
+  );
+};
+
+export default Home;
